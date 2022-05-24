56563b213c795ae4d36c9901c7d2d40204a7bf2f
Terrance Brown <terrancem.brown@gmail.com>
2022-05-24 00:57:06 -0500

    name casing for files and modules

--- components/SignUpComponent.js
+++ components/SignUpComponent.js
@@ -1,5 +1,5 @@
 import Link from 'next/link'
-import Image from 'next/Image'
+import Image from 'next/image'
 import { useState } from 'react'
 import { AuthFunctions } from '../src/AuthContext'
 import { setDoc, doc, getDoc, collection } from 'firebase/firestore'
