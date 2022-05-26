f3dec1e78948b13d8a46a632b753dd35b6cf3251
Terrance Brown <terrancem.brown@gmail.com>
2022-05-26 09:09:37 -0500

    Updated Environment Variables

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@ import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyAZ7xAGVvDownh2d-3E3G0pw3eXvNU14JA',
-  authDomain: 'proj-apex-matchmaker.firebaseapp.com',
-  projectId: 'proj-apex-matchmaker',
-  storageBucket: 'proj-apex-matchmaker.appspot.com',
-  messagingSenderId: '955861648251',
-  appId: '1:955861648251:web:c106181b9952ecedccb3de',
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
