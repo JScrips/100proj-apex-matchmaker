b62e5c930760fb269f1b4831dd7c9f8ea290b474
Terrance Brown <terrancem.brown@gmail.com>
2022-05-26 14:36:35 -0500

    fiddle with env variables

--- firebase-config.js
+++ firebase-config.js
@@ -3,8 +3,7 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 
-console.log(process.env.API_KEY)
-
+console.log(process.env)
 // const firebaseConfig = {
 //   apiKey: 'AIzaSyAZ7xAGVvDownh2d-3E3G0pw3eXvNU14JA',
 //   authDomain: 'proj-apex-matchmaker.firebaseapp.com',
@@ -15,12 +14,12 @@ console.log(process.env.API_KEY)
 // }
 
 const firebaseConfig = {
-  apiKey: process.env.API_KEY,
-  authDomain: process.env.AUTH_DOMAIN,
-  projectId: process.env.PROJECT_ID,
-  storageBucket: process.env.STORAGE_BUCKET,
-  messagingSenderId: process.env.MESSAGING_SENDER_ID,
-  appId: process.env.APP_ID,
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
 }
 
 const app = initializeApp(firebaseConfig)
