f5cfebe0cf2cdfa69cde600e5edc0c5ed2bfea89
Terrance Brown <terrancem.brown@gmail.com>
2022-05-26 14:05:11 -0500

    fiddling with env variables

--- firebase-config.js
+++ firebase-config.js
@@ -3,23 +3,25 @@ import { getFirestore } from 'firebase/firestore'
 import { getAuth } from 'firebase/auth'
 import { getStorage } from 'firebase/storage'
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAZ7xAGVvDownh2d-3E3G0pw3eXvNU14JA',
-  authDomain: 'proj-apex-matchmaker.firebaseapp.com',
-  projectId: 'proj-apex-matchmaker',
-  storageBucket: 'proj-apex-matchmaker.appspot.com',
-  messagingSenderId: '955861648251',
-  appId: '1:955861648251:web:c106181b9952ecedccb3de',
-}
+console.log(process.env.API_KEY)
 
 // const firebaseConfig = {
-//   apiKey: process.env.NEXT_PUBLIC_API_KEY,
-//   authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//   projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-//   storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//   messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-//   appId: process.env.NEXT_PUBLIC_APP_ID,
+//   apiKey: 'AIzaSyAZ7xAGVvDownh2d-3E3G0pw3eXvNU14JA',
+//   authDomain: 'proj-apex-matchmaker.firebaseapp.com',
+//   projectId: 'proj-apex-matchmaker',
+//   storageBucket: 'proj-apex-matchmaker.appspot.com',
+//   messagingSenderId: '955861648251',
+//   appId: '1:955861648251:web:c106181b9952ecedccb3de',
 // }
+
+const firebaseConfig = {
+  apiKey: process.env.API_KEY,
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID,
+}
 
 const app = initializeApp(firebaseConfig)
 export const db = getFirestore(app)
